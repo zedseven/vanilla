083e79f62fce47c494a30a0563a41212ccd81fb5
Zacchary Dempsey-Plante <zacchary.dempseyplante@gmail.com>
2020-08-14 21:04:45 -0400

    Set up formatting.

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,7 +1,7 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
 use neon::prelude::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 mod merge_files;
 
@@ -10,7 +10,10 @@ const VERSION_MID: u8 = 1;
 const VERSION_MIN: u8 = 0;
 
 fn version(mut cx: FunctionContext) -> JsResult<JsString> {
-	Ok(cx.string(format!("Vanilla version: v{}.{}.{}", VERSION_MAX, VERSION_MID, VERSION_MIN)))
+	Ok(cx.string(format!(
+		"Vanilla version: v{}.{}.{}",
+		VERSION_MAX, VERSION_MID, VERSION_MIN
+	)))
 }
 
 fn vanilla(mut cx: FunctionContext) -> JsResult<JsString> {
@@ -20,11 +23,11 @@ fn vanilla(mut cx: FunctionContext) -> JsResult<JsString> {
 		Err(err) => {
 			eprintln!("{:?}", err);
 			// TODO: return err
-			return Ok(cx.string(format!("An error occurred: {:?}", err)))
-		},
-		_ => mergeable_files = mergeable_files_res.unwrap()
+			return Ok(cx.string(format!("An error occurred: {:?}", err)));
+		}
+		_ => mergeable_files = mergeable_files_res.unwrap(),
 	}
-	
+
 	let base_path = "testFiles/base/vehicles.meta";
 	let new_path = "testFiles/new/vehicles.meta";
 	let out_path = "testFiles/merged/vehicles.meta";
@@ -32,7 +35,7 @@ fn vanilla(mut cx: FunctionContext) -> JsResult<JsString> {
 	if merge_res.is_err() {
 		eprintln!("{:?}", merge_res.unwrap());
 	}
-	
+
 	Ok(cx.string("lol"))
 }
 
@@ -46,16 +49,21 @@ fn merge_files(mut cx: FunctionContext) -> JsResult<JsBoolean> {
 	match mergeable_files_res {
 		Err(err) => {
 			eprintln!("{:?}", err);
-			return Ok(cx.boolean(false))
-		},
-		_ => mergeable_files = mergeable_files_res.unwrap()
+			return Ok(cx.boolean(false));
+		}
+		_ => mergeable_files = mergeable_files_res.unwrap(),
 	}
-	
-	let merge_res = merge_files::merge(&mergeable_files, base_path.as_str(), new_path.as_str(), out_path.as_str()); 
+
+	let merge_res = merge_files::merge(
+		&mergeable_files,
+		base_path.as_str(),
+		new_path.as_str(),
+		out_path.as_str(),
+	);
 	if merge_res.is_err() {
-		return Ok(cx.boolean(false))
+		return Ok(cx.boolean(false));
 	}
-	return Ok(cx.boolean(true))
+	return Ok(cx.boolean(true));
 }
 
 register_module!(mut cx, {
--- native/src/merge_files.rs
+++ native/src/merge_files.rs
@@ -1,22 +1,22 @@
-use thiserror::Error;
+use serde::Deserialize;
+use std::borrow::Cow;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::Read;
 use std::path::Path;
-use xmltree::{ Element, XMLNode };
-use serde::Deserialize;
-use std::borrow::Cow;
+use thiserror::Error;
 use xml::EmitterConfig;
+use xmltree::{Element, XMLNode};
 
 #[derive(Deserialize, Debug)]
 pub struct MergeableFile {
 	file_name: String,
-	parent_tags: Vec<String>
+	parent_tags: Vec<String>,
 }
 
 #[derive(Deserialize, Debug)]
 struct TypesVec {
-	types: Vec<MergeableFile>
+	types: Vec<MergeableFile>,
 }
 
 #[derive(Error, Debug)]
@@ -34,7 +34,7 @@ pub enum MergeError {
 	#[error("There was a parse error in one of the provided files: {0}")]
 	XmlParseError(#[from] xmltree::ParseError),
 	#[error("There was a write error when writing an XML document: {0}")]
-	XmlWriteError(#[from] xmltree::Error)
+	XmlWriteError(#[from] xmltree::Error),
 }
 
 const CONFIG_PATH: &str = "config/mergeFiles.toml";
@@ -48,7 +48,7 @@ const XML_WRITE_CONFIG: xmltree::EmitterConfig = EmitterConfig {
 	cdata_to_characters: false,
 	keep_element_names_stack: true,
 	autopad_comments: true,
-	pad_self_closing: true
+	pad_self_closing: true,
 };
 
 pub fn init() -> Result<Vec<MergeableFile>, MergeError> {
@@ -57,34 +57,39 @@ pub fn init() -> Result<Vec<MergeableFile>, MergeError> {
 	let mut contents = String::new();
 	config_buf.read_to_string(&mut contents)?;
 	let config_value: TypesVec = toml::from_str(&*contents)?;
-	
-	return Ok(config_value.types)
+
+	return Ok(config_value.types);
 }
 
-pub fn merge(file_types: &Vec<MergeableFile>, base_path: &str, additive_path: &str, out_path: &str) -> Result<(), MergeError> {
+pub fn merge(
+	file_types: &Vec<MergeableFile>,
+	base_path: &str,
+	additive_path: &str,
+	out_path: &str,
+) -> Result<(), MergeError> {
 	// Validate base and additive paths
 	let base_name = Path::new(base_path).file_name();
 	if base_name.is_none() {
 		return Err(MergeError::BadFilePath);
 	}
-	let base_name = Path::new(base_path).file_name().unwrap().to_str();
+	let base_name = base_name.unwrap().to_str();
 	if base_name.is_none() {
 		return Err(MergeError::BadFilePath);
 	}
-	let base_name = Path::new(base_path).file_name().unwrap().to_str().unwrap();
+	let base_name = base_name.unwrap();
 	let additive_name = Path::new(additive_path).file_name();
 	if additive_name.is_none() {
 		return Err(MergeError::BadFilePath);
 	}
-	let additive_name = Path::new(additive_path).file_name().unwrap().to_str();
+	let additive_name = additive_name.unwrap().to_str();
 	if additive_name.is_none() {
 		return Err(MergeError::BadFilePath);
 	}
-	let additive_name = Path::new(additive_path).file_name().unwrap().to_str().unwrap();
+	let additive_name = additive_name.unwrap();
 	if base_name != additive_name {
 		return Err(MergeError::DifferentInputFiles);
 	}
-	
+
 	// Get the file type definition
 	let mut f_type = None;
 	for check_file in file_types {
@@ -97,19 +102,19 @@ pub fn merge(file_types: &Vec<MergeableFile>, base_path: &str, additive_path: &s
 		return Err(MergeError::FileTypeNotInConfig);
 	}
 	let f_type = f_type.unwrap();
-	
+
 	// Prepare the files
 	let base = File::open(base_path)?;
 	let base_buf = BufReader::new(base);
 	let additive = File::open(additive_path)?;
 	let additive_buf = BufReader::new(additive);
-	
+
 	let parent_tag_count = f_type.parent_tags.len();
 	let mut item_nodes: Vec<Vec<&XMLNode>> = Vec::new();
 	for _ in 0..parent_tag_count {
 		item_nodes.push(Vec::new())
 	}
-	
+
 	// Build a collection of nodes from the additive file to add to the base
 	let additive_top_element = Element::parse(additive_buf)?;
 	for i in 0..parent_tag_count {
@@ -123,7 +128,7 @@ pub fn merge(file_types: &Vec<MergeableFile>, base_path: &str, additive_path: &s
 			}
 		}
 	}
-	
+
 	// Go through the base and add in the additive's nodes
 	let mut base_top_element = Element::parse(base_buf)?;
 	for i in 0..parent_tag_count {
@@ -137,9 +142,9 @@ pub fn merge(file_types: &Vec<MergeableFile>, base_path: &str, additive_path: &s
 			}
 		}
 	}
-	
+
 	let buffer = File::create(out_path)?;
 	base_top_element.write_with_config(buffer, XML_WRITE_CONFIG)?;
-	
-	return Ok(())
+
+	return Ok(());
 }
